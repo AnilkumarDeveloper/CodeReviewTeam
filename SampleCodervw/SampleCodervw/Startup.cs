671a3642e7d69d2fc50baea6fe11fe69447c9e51
v-animar <v-animar@HYDPCM390080D>
2016-06-24 12:12:23 +0530

    v1.1

---
+++ SampleCodervw/SampleCodervw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleCodervw.Startup))]
+namespace SampleCodervw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
